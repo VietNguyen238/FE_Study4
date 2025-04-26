478a1ec84c26a9cb66cecc6864b17f36e515c478
Viet Nguyen <vietdz2308@gmail.com>
2025-04-27 01:41:10 +0700

    Feat: Create Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import Slideshow from "../components/ui/Slideshow";
 import { images, image, itemsCard, topic } from "../config/data";
 import AdviseForm from "../components/ui/AdviseForm";
 import TopicCard from "../components/ui/TopicCard";
+import Button from "../components/ui/Button";
 
 export default function Home() {
   return (
@@ -85,7 +86,103 @@ export default function Home() {
           ))}
         </div>
       </div>
+      <div className="flex flex-col justify-center items-center mt-20 mb-10">
+        <div className="font-semibold text-[28px] mb-4 text-black">
+          Luyện thi IELTS, TOEIC, HSK, TOPIK ... không giới hạn
+        </div>
+        <div className="flex text-black text-[16px] font-normal mb-12 text-center">
+          <div className="">
+            <div className="relative w-[105px]">
+              <div className="absolute left-16">
+                <img
+                  className="h-[35px] w-[35px] border-[2px] border-white bg-cover rounded-full"
+                  src="./../src/assets/images/img_u5.webp"
+                />
+              </div>
+              <div className="absolute left-12">
+                <img
+                  className="h-[35px] w-[35px] border-[2px] border-white bg-cover rounded-full"
+                  src="./../src/assets/images/img_u4.webp"
+                />
+              </div>
+              <div className="absolute left-8">
+                <img
+                  className="h-[35px] w-[35px] border-[2px] border-white bg-cover rounded-full"
+                  src="./../src/assets/images/img_u2.jpg"
+                />
+              </div>
+              <div className="absolute left-4">
+                <img
+                  className="h-[35px] w-[35px] border-[2px] border-white bg-cover rounded-full"
+                  src="./../src/assets/images/img_u3.jpg"
+                />
+              </div>
+              <div className="absolute left-0">
+                <img
+                  className="h-[35px] w-[35px] border-[2px] border-white bg-cover rounded-full"
+                  src="./../src/assets/images/img_u1.jpg"
+                />
+              </div>
+            </div>
+          </div>
+          <i className="leading-loose i">
+            và hơn 1 triệu học viên hoạt động mỗi tháng
+          </i>
+        </div>
+        <div className="grid grid-cols-12 w-page">
+          <img
+            src="./../src/assets/images/img_home_community.webp"
+            alt="img_home_community"
+            className="col-span-5 px-16"
+          />
+          <div className="col-span-7">
+            <div>
+              <div className="flex mb-3 gap-2 items-start">
+                <img
+                  src="./../src/assets/svgs/ic_tick.svg"
+                  alt="ic_tick"
+                  className="w-4 mt-1"
+                />
+                Cộng đồng học tiếng Anh và luyện thi sôi nổi với hơn 1 triệu học
+                viên mỗi tháng
+              </div>
+              <div className="flex mb-3 gap-2 items-start">
+                <img
+                  src="./../src/assets/svgs/ic_tick.svg"
+                  alt="ic_tick"
+                  className="w-4 mt-1"
+                />
+                Đặt câu hỏi cho đội ngũ trợ giảng cũng như các học viên khác để
+                nhận giải đáp sau 30 phút
+              </div>
+              <div className="flex mb-3 gap-2 items-start">
+                <img
+                  src="./../src/assets/svgs/ic_tick.svg"
+                  alt="ic_tick"
+                  className="w-4 mt-1"
+                />
+                Chia sẻ kinh nghiệm học tập và làm bài thi với các thành viên
+                khác
+              </div>
+              <div className="flex mb-3 gap-2 items-start">
+                <img
+                  src="./../src/assets/svgs/ic_tick.svg"
+                  alt="ic_tick"
+                  className="w-4 mt-1"
+                />
+                Luyện tập kỹ năng nói &amp; viết và nhận được nhận xét, chấm
+                điểm từ bạn học và giáo viên
+              </div>
 
+              <div className="mb-4 mt-6">
+                <a className="btn btn-block btn-primary" href="/register">
+                  <Button title="Bắt đầu luyện thi" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div
         className="flex justify-center py-14 bg-slate-400 w-full mt-12"
         style={{
